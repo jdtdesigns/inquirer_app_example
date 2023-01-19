e02f6abe40a9f73ad15383c5a877dbc6fc8d3a1f
JD Tadlock <jt7903@gmail.com>
2023-01-19 15:29:28 -0500

    finished module version

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
 
-inquirer.prompt([
-  {
-    name: 'file_name',
-    message: 'What would you like the file name to be?'
-  },
-  {
-    name: 'content',
-    message: 'What content would you like to add to the file?'
-  }
-]).then(data => {
-  fs.writeFile(data.file_name + '.txt', data.content, (err) => {
-    if (err) return console.log(err);
+const prompts = require('./lib/promptUser');
 
-    console.log(`You have successfully created the ${data.file_name} file`);
-    console.log(`The contents of the file are:\n${data.content}`);
-  });
-});
+prompts.cyclePrompt();
